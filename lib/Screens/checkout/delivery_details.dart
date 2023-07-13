a5d7b06a592ac4985125b2d03574edd2b3a680bc
Shubham7823 <114138639+Shubham7823@users.noreply.github.com>
2023-07-14 02:43:32 +0530

    Add files via upload

---
+++ lib/Screens/checkout/delivery_details.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:medquick_minor/Screens/check_out/add_delivery_address/add_delivery_address.dart';
+import 'package:medquick_minor/Screens/check_out/payment_summary/payment_summary.dart';
+import 'package:medquick_minor/Screens/check_out/single_delivery_item.dart';
+import 'package:provider/provider.dart';
+
+import '../../models/delivery_address_model.dart';
+import '../../providers/checkout_provider.dart';
+
+class DeliveryDetails extends StatefulWidget {
+  @override
+  _DeliveryDetailsState createState() => _DeliveryDetailsState();
+}
+
+class _DeliveryDetailsState extends State<DeliveryDetails> {
+  late DeliveryAddressModel value;
+  @override
+  Widget build(BuildContext context) {
+    CheckoutProvider deliveryAddressProvider = Provider.of(context);
+    deliveryAddressProvider.getDeliveryAddressData();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 164, 169, 225),
+        title: Text(
+          "Delivery Details",
+        ),
+        titleTextStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 19),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color.fromARGB(255, 91, 173, 240),
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => AddDeliveryAddress(),
+            ),
+          );
+        },
+      ),
+      bottomNavigationBar: Container(
+        // width: 160,
+        height: 48,
+        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        child: MaterialButton(
+          child: deliveryAddressProvider.getDeliveryAddressList.isEmpty
+              ? Text("Add new Address")
+              : Text("Payment Summary"),
+          onPressed: () {
+            deliveryAddressProvider.getDeliveryAddressList.isEmpty
+                ? Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => AddDeliveryAddress(),
+                    ),
+                  )
+                : Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => PaymentSummary(
+                        deliverAddressList: value,
+                      ),
+                    ),
+                  );
+          },
+          color: Colors.yellow,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(
+              30,
+            ),
+          ),
+        ),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text("Deliver To"),
+          ),
+          Divider(
+            height: 1,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          deliveryAddressProvider.getDeliveryAddressList.isEmpty
+              ? Center(
+                  child: Container(
+                    child: Center(
+                      child: Text("No Data"),
+                    ),
+                  ),
+                )
+              : Column(
+                  children: deliveryAddressProvider.getDeliveryAddressList
+                      .map<Widget>((e) {
+                    setState(() {
+                      value = e;
+                    });
+                    return SingleDeliveryItem(
+                      address: " pincode ${e.pinCode}",
+                      title: "${e.firstName} ${e.lastName}",
+                      number: e.mobileNo,
+                      addressType: e.addressType == "AddressTypes.Home"
+                          ? "Home"
+                          : e.addressType == "AddressTypes.Other"
+                              ? "Other"
+                              : "Work",
+                    );
+                  }).toList(),
+                )
+        ],
+      ),
+    );
+  }
+}
